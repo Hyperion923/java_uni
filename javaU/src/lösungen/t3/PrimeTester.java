6c079dc02aad225a8063d8594a9e9f11ff08dab9
cap <christian@apsel-mv.de>
2019-08-09 13:43:53 +0200

    update t3

--- javaU/src/lösungen/t3/PrimeTester.java
+++ javaU/src/lösungen/t3/PrimeTester.java
@@ -6,23 +6,35 @@ import java.util.stream.IntStream;
 public class PrimeTester {
     public static void main(String[] args) {
         PrimeTester primeTester = new PrimeTester();
-        primeTester.properDivisors(13);
+        System.out.println(primeTester.isPrime(2));
     }
     public void divisors(int n) {
         if (n >= 1) {
-            System.out.println(IntStream.rangeClosed(1, n).filter(i -> n%i == 0).mapToObj(i ->""+i+"\n").collect(Collectors.joining()));
+            String output =("Teiler von "+ n + " sind " + (IntStream.rangeClosed(1, n).filter(i -> n%i == 0).mapToObj(i ->""+i+", ").collect(Collectors.joining())));
+            System.out.println(output.substring(0, output.length()-2));
         }
     }
 
     public void properDivisors(int n) {
-        if (n >= 1) {
-            System.out.println(IntStream.rangeClosed(2, (n-1)).filter(i -> n%i == 0).mapToObj(i ->""+i+"\n").collect(Collectors.joining()));
+         if (isPrime(n)|| n == 1){
+            System.out.println("Echte Teiler von "+ n + " sind ");
+        }else if (n >= 1) {
+            String output =("Echte Teiler von "+ n + " sind " + IntStream.rangeClosed(2, (n-1)).filter(i -> n%i == 0).mapToObj(i ->""+i+", ").collect(Collectors.joining()));
+            System.out.println(output.substring(0, output.length()-2));
         }
 
     }
 
     public boolean isPrime(int n) {
-        return n > 2 && IntStream.rangeClosed(2, (int) Math.sqrt(n)).noneMatch(o -> (n % o == 0));
+        //fuvck the law
+        while (true){
+            break;
+        }
+        //fuck the law
+        for (;;){
+            break;
+        }
+        return n >= 2 && IntStream.rangeClosed(2, (int) Math.sqrt(n)).noneMatch(o -> (n % o == 0));
     }
 
 }
