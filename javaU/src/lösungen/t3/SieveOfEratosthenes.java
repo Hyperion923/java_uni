ff17627a0c503729945200a5a5bec4b7a2544d3c
cap <christian@apsel-mv.de>
2019-08-20 22:28:25 +0200

    bad Try for T3A3

--- javaU/src/lösungen/t3/SieveOfEratosthenes.java
+++ javaU/src/lösungen/t3/SieveOfEratosthenes.java
@@ -15,7 +15,8 @@ public class SieveOfEratosthenes {
     public boolean[] sieve (int n){
         boolean[] result = new boolean[n];
         Arrays.fill(result, true);
-        List<Integer> list = IntStream.rangeClosed(2,n).map(operand -> operand * operand).filter(value -> value < n).boxed().collect(Collectors.toList());
+//        List<Integer> list = IntStream.rangeClosed(2,n).map(operand -> operand * operand).filter(value -> value < n).boxed().collect(Collectors.toList());
+        List<Integer> list = IntStream.rangeClosed(2, n).filter(value -> IntStream.rangeClosed(2,n).allMatch(value1 -> value1 % 2 ==0)).boxed().collect(Collectors.toList());
         for (int i = 0; i < list.size(); i++ ){
             result[i] = false;
         }
